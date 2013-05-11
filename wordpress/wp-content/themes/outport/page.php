5f0dd24ab03c3806ed66667f96b942a99fd0f036
Lionel Mann <lionelmann@gmail.com>
2013-05-11 14:27:30 -0400

    index

--- wordpress/wp-content/themes/outport/page.php
+++ wordpress/wp-content/themes/outport/page.php
@@ -3,11 +3,13 @@
 <div class="row">
 
     <div class="large-8 offset-by-1 columns">
-        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-            <h1><?php the_title(); ?></h1>
-            <hr>
-            <?php the_content(); ?>
-        <?php endwhile; endif; //Loop ends ?>
+        <article>
+            <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+                <h1><?php the_title(); ?></h1>
+                <hr>
+                <?php the_content(); ?>
+            <?php endwhile; endif; //Loop ends ?>
+    </article>
 	</div>
 
 <?php get_sidebar(); ?>
