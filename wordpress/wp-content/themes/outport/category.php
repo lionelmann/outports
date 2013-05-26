e0d27cbedd72976c78b1dc45bdda47b115f22467
Lionel Mann <lionelmann@gmail.com>
2013-05-26 16:21:30 -0400

    permalink

--- wordpress/wp-content/themes/outport/category.php
+++ wordpress/wp-content/themes/outport/category.php
@@ -9,7 +9,7 @@
             ?>
             <hr>
             <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-            <h2><a href="#"><?php the_title(); ?></a></h2>
+            <h2><a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a></h2>
             <hr>
                 
                 
