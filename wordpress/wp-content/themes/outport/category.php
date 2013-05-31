d7dd64b9c9fcd51687d92ccbeb2241663657fc60
Lionel Mann <lionelmann@gmail.com>
2013-05-30 21:26:13 -0400

    category template change

--- wordpress/wp-content/themes/outport/category.php
+++ wordpress/wp-content/themes/outport/category.php
@@ -8,15 +8,25 @@
                 echo '<h1>' . $category[0]->cat_name . '</h1>';
             ?>
             <hr>
-            <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+            <?php
+                $args = array(
+                    'post_type' => array('post','community_post')
+                    );
+
+                $query = new WP_Query($args);
+            ?>
+
+            <?php if (have_posts() ) : while (  $query->have_posts() ) :  $query->the_post(); ?>
+
             <h2><a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a></h2>
             <hr>
                 
                 
-                <?php if ( !empty( $post->post_excerpt ) ) : ?>
-                    <p class="large"><?php echo excerpt(999); ?></p>
-                    <br>
-                <?php endif; ?>
+            <?php if ( !empty( $post->post_excerpt ) ) : ?>
+                <p class="large"><?php echo excerpt(999); ?></p>
+                <br>
+            <?php endif; ?>
 
             <?php endwhile; endif; //Loop ends ?>
 
