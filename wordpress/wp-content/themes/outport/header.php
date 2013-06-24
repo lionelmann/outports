57aea94048d6f06a48bd59e06e1d5c2362cbc060
Lionel Mann <lionelmann@gmail.com>
2013-06-23 22:37:36 -0400

    google analytics

--- wordpress/wp-content/themes/outport/header.php
+++ wordpress/wp-content/themes/outport/header.php
@@ -54,11 +54,7 @@ $(document).ready(function(){
 });
 </script>
 
-</head>
-
-<body>
-
-    <script>
+<script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -69,6 +65,9 @@ $(document).ready(function(){
 
 </script>
 
+</head>
+
+<body>
     <header>
         <div class="row">
 	       <div class="large-12 large-centered columns" style="text-align: center">
